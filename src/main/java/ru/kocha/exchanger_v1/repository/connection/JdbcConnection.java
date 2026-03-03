1f211ddf9f6f271eb46c8d2c7cc9f34546be0ab7
Кочетков Александр <ko4etkovalexandr@gmail.com>
2026-03-03 22:31:05 +0300

    refactor Jdbc connection

--- src/main/java/ru/kocha/exchanger_v1/repository/connection/JdbcConnection.java
+++ src/main/java/ru/kocha/exchanger_v1/repository/connection/JdbcConnection.java
@@ -8,9 +8,13 @@ import java.sql.SQLException;
 
 public final class JdbcConnection {
 
-    private static final String URL = System.getenv("POSTGRES_URL");
-    private static final String USERNAME = System.getenv("POSTGRES_USERNAME");
-    private static final String PASSWORD = System.getenv("POSTGRES_PASSWORD");
+    private static final String URL = getRequiredEnv("POSTGRES_URL");
+    private static final String USERNAME = getRequiredEnv("POSTGRES_USERNAME");
+    private static final String PASSWORD = getRequiredEnv("POSTGRES_PASSWORD");
+
+    private static final Long THIRTY_SECOND = 30L;
+    private static final Long TEN_MINUTE = 60L;
+    private static final Long THIRTY_MINUTE = 1800L;
 
     private static HikariConfig config = new HikariConfig();
     private static HikariDataSource ds;
@@ -20,11 +24,40 @@ public final class JdbcConnection {
         config.setUsername(USERNAME);
         config.setPassword(PASSWORD);
         config.setDriverClassName("org.postgresql.Driver");
+
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(2);
+        config.setConnectionTimeout(THIRTY_SECOND);
+        config.setIdleTimeout(TEN_MINUTE);
+        config.setMaxLifetime(THIRTY_MINUTE);
+
         ds = new HikariDataSource(config);
+
+        try (Connection connection = ds.getConnection()) {
+            System.out.println("Database connection pool initialized successfully");
+        } catch (SQLException e) {
+            System.err.println("Database connection pool initialization failed");
+            throw new RuntimeException("Database connection failed" + e);
+        }
     }
 
     public static Connection getConnection() throws SQLException {
        return ds.getConnection();
+    }
+
+    private static String getRequiredEnv(String env) {
+        String value = System.getenv(env);
+        if (value == null || value.isEmpty()) {
+            throw new IllegalArgumentException("Missing required environment variable " + env);
+        }
+        return value;
+    }
+
+    public static void closeConnection() {
+        if (ds != null || !ds.isClosed()) {
+            ds.close();
+            System.out.println("Database connection closed successfully");
+        }
     }
 
     private  JdbcConnection() {}
